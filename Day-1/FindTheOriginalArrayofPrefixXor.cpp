a619162f349f2b291ff2b3456ea3882c77f8ea6b
Shivprasad Nikam <116971586+shivprasadnikam@users.noreply.github.com>
2023-01-24 13:42:36 +0530

    Create FindTheOriginalArrayofPrefixXor.cpp

---
+++ Day-1/FindTheOriginalArrayofPrefixXor.cpp
@@ -0,0 +1,19 @@
+
+
+                                                          // Find The Original Array of Prefix Xor  //
+                                                          
+                                                          
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int>ans;
+        int temp=pref[0];
+        ans.push_back(pref[0]);
+        for(int i=1; i<pref.size(); i++)
+        {
+          
+            ans.push_back(pref[i]^pref[i-1]);
+        }
+        return ans;
+    }
+};
